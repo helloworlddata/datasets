828bb043653b9dfcf938034fb3d46470b63ba0fe
Dan Nguyen <dansonguyen@gmail.com>
2016-06-06 19:52:27 -0700

    rearrainging

---
+++ pages/nypd-stop-question-and-frisk/scripts/python/data_settings.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+FETCHED_DATA_DIR = Path('data', 'fetched')
+FETCHED_CSV_DIR = FETCHED_DATA_DIR / 'csv'
+FETCHED_CSV_DIR.mkdir(exist_ok=True, parents=True)
+FETCHED_ZIP_DIR = FETCHED_DATA_DIR / 'zip'
+FETCHED_ZIP_DIR.mkdir(exist_ok=True, parents=True)
