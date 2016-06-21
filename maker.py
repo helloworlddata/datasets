63ebbacfd998521453a505408f359d54e3e69db6
dannguyen <dansonguyen@gmail.com>
2016-06-20 22:45:13 -0700

    huh

---
+++ maker.py
@@ -0,0 +1,14 @@
+"""
+Just a script to build out a quick README
+"""
+
+from pathlib import Path
+working_path = Path('datasets', 'nypd-stop-question-and-frisk')
+dest_path = working_path / 'README.md'
+
+
+
+
+txt = "ooga booga"
+
+dest_path.write_text(txt)
